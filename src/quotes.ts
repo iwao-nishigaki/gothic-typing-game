abc8a13b213a03bb35d6c5a991e2aa7473e1d623
iwao <igpi0000igpi@gmail.com>
2026-05-14 17:46:05 +0900

    first commit

---
+++ src/quotes.ts
@@ -0,0 +1,12 @@
+export const quotes = [
+  {
+    text: "生まれてこなかった方がよかった",
+    reading: "umaretekonakattahougayokatta",
+    author: "アーサー・ショーペンハウアー"
+  },
+  {
+    text: "我々は生きるために生まれたのではない",
+    reading: "warewarehaikirutameniumaretanodehanai",
+    author: "ブレーズ・パスカル"
+  }
+];
