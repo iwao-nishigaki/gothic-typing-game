bf86a6a2fc0ec75fe2a4e0ea55db26eff85da2fa
iwao <igpi0000igpi@gmail.com>
2026-05-15 17:11:15 +0900

    add many quotes

--- src/quotes.ts
+++ src/quotes.ts
@@ -1,12 +1,67 @@
 export const quotes = [
   {
-    text: "生まれてこなかった方がよかった",
-    reading: "umaretekonakattahougayokatta",
-    author: "アーサー・ショーペンハウアー"
+    text: "私の存在は偶然に過ぎない。なぜそんなに全てを深刻にとらえるのか",
+    reading: "watashinosonzaihaguuzenisuginai.nazesonnanisubetewoshinkokunitoraerunoka",
+    author: "エミール・シオラン"
   },
   {
-    text: "我々は生きるために生まれたのではない",
-    reading: "warewarehaikirutameniumaretanodehanai",
-    author: "ブレーズ・パスカル"
+    text: "人間とは地球の癌だ",
+    reading: "ningentohachikyuunoganda",
+    author: "エミール・シオラン"
+  },
+  {
+    text: "老いとは、要するに生きたことに対する懲罰にほかならぬ。",
+    reading: "oitohayousuruniikitakotonitaisuruchoubatsunihokanaranu",
+    author: "エミール・シオラン"
+  },
+  {
+    text: "生れたという屈辱を、いまだに消化しかねている。",
+    reading: "umaretatoiukutsujokuwoimadanishoukashikaneteiru",
+    author: "エミール・シオラン"
+  },
+  {
+    text: "われわれが生きて行けるのは、ただわれわれの想像力と記憶力が貧弱だからにすぎない",
+    reading: "warewaregaikiteikerunowatadawarewarenosouzouryokutokiokuryokugahinjyakudakaranisuginai",
+    author: "エミール・シオラン"
+  },
+  {
+    text: "生きるとは追いつめられることだ",
+    reading: "ikirutohaoitsumerarerukotoda",
+    author: "エミール・シオラン"
+  },
+  {
+    text: "希望とは未来に対してつく嘘である",
+    reading: "kiboutohamirainitaisitetsukuusodearu",
+    author: "エミール・シオラン"
+  },
+  {
+    text: "生にはなんの意味もないという事実は、生きる理由の一つになる",
+    reading: "seinihanann noimimonaitoiujijitsuhaikiruriyuunohitotsuninaru",
+    author: "エミール・シオラン"
+  },
+  {
+    text: "両親とは、いずれも無責任な者か人殺しだ。",
+    reading: "ryoushintohaizuremomusekininnnamonokahitogoroshida",
+    author: "エミール・シオラン"
+  },
+  {
+    text: "同情心があれば、私たちは「人の親」にはなれまい。",
+    reading: "doujoushingaarebawatashitachihahitonooyanihanaremai",
+    author: "エミール・シオラン"
+  },
+  {
+    text: "ただ生まれなければよかったのにと思っているだけだ。",
+    reading: "tadaumarenakerebayokattanonitoomotteirudakeda",
+    author: "エミール・シオラン"
+  },
+  {
+    text: "結局のところ、私たちが自殺しないのは、自殺の理由がありすぎるからだ。",
+    reading: "kekkyokunotokorowatashitachigajisatsushinainohajisatsunoriyuugaarisugirukarada",
+    author: "エミール・シオラン"
+  },
+  {
+    text: "私には疲労という才能がある。",
+    reading: "watashinihahiroutoiusainougaaru",
+    author: "エミール・シオラン"
   }
 ];
